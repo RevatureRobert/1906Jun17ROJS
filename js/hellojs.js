3b5d694084e5ee95a7c7c8079e505a49c7ef1102
connellrobert <robert.lel.connell@gmail.com>
2019-06-20 16:54:03 -0400

    more js

--- js/hellojs.js
+++ js/hellojs.js
@@ -39,6 +39,12 @@ datatypes in js
         Object
         Symbol
 */
+//////////////PRIMITIVE DATATYPES in javascript
+//          number, string, boolean, null, undefined, symbol
+
+////////////REFERENCED TYPES
+//          object, function, array
+
 
 //how can we declare a variable?
 // var v1;
@@ -58,7 +64,7 @@ var and let are mutable
 
 Scope
     Global      is accessible throughout the program
-    Function
+    Function    is accessible throughout the function
     Local       is accessible throughout the local block of code
 */
 
@@ -177,22 +183,22 @@ falsey values in js
 //     console.log("it is 3: " + x);
 // }
 
-//for([declare variable][semicolon][check value 
-    //(give stopping point)][semicolon][increment])
-        //{logic}
+//for([declare variable][semicolon][check value
+//(give stopping point)][semicolon][increment])
+//{logic}
 
 /*for loops will perform a function for each iteration
     of the loop
     */
 
-//         const x =0;
+//         let x =0;
 // for(let x =1;x<100;x++){
 //     console.log(x);
 // }
 
 //lets say i want to iterate through a loop
-    //as long as a condition is true
-    /*
+//as long as a condition is true
+/*
     while loops will iterate through a loop as long
     as the condition, if it is never true, it will never
     run
@@ -212,3 +218,163 @@ falsey values in js
             watch out for
             infinite loops
  */
+// alert("this is js")
+// console.log('this is a log')
+// console.warn('this is a warning')
+// console.error('this is an error')
+// console.info('this is an informative line')
+// console.trace();
+// let x =4;
+// let X =6;
+// console.log(X);
+
+//  another keyword that you will need to know
+
+//this is how you declare a function
+function myFunc() {
+  console.log("this is the new function");
+}
+let x = 77;
+function myFunc2(num) {
+  console.error(num);
+}
+//this is how you call a function
+// myFunc();
+
+function myFunc3(haroldTheOldCat) {
+  console.log(haroldTheOldCat);
+}
+
+function myFunc4() {
+  for (let i = 0; i < 5; i++) {}
+  for (var k = 0; k < 5; k++) {
+    a = 15;
+  }
+  var k = 20;
+  console.log(k);
+  console.log(a);
+}
+/*
+    var has global and function scope
+    let and const has global local and function scope
+*/
+//var meVar = 3; //this is global
+// let meVar = 3; //this is global
+const meVar = 3; //this is global
+
+function newFunc() {
+  // var itHappens = true;//this is function scope
+  // let itHappens = true; //this is function scope
+  const itHappens = true; //this is function scope
+  if (itHappens) {
+    var panic = "yes"; //this is function scope
+    // let panic = 'yes we are panicking'; //this is local scope
+    // const panic = 'we are constantly panicking'; //this is local scope
+    // console.log(panic)
+  }
+  console.log(panic);
+}
+// newFunc();
+
+//welcome to closure
+
+// let driveCar =(function (){
+//     let miles=0;
+//     console.log('We have purchased the car')
+//     return function(){
+//         console.log('we are driving the car')
+//         return ++miles;
+//     }
+// })();
+
+let newArr = [];
+let kangaroo = [5, 10, 16, 19, 3];
+//a data structure consisting of a collection of elements
+//      (values or variables), each identified by
+//      at least one array index or key
+
+starfisheries = [5, 10, "mongoose", 19, "bravo", false, NaN, undefined];
+
+// function loopthru(arr) {
+//   for (let x = 0; x < arr.length; x++) {
+//     console.log(arr[x]);
+//   }
+// }
+
+//this is known as a for of loop
+//  a for of loop will grab each
+//   element of an array
+function loopthruBetter(arr){
+    for(let yoda of arr){
+        console.log(yoda)
+    }
+}
+
+//this is a for in loop
+// a for in loop will get the 
+//  index of each element
+function loopthruBetter2(arr){
+    for(let yoda in arr){
+        console.log(yoda)
+    }
+}
+
+
+
+// A object is a set of states and behaviors
+//  the syntax for an object in JS is:
+
+let yoda={
+    name:'yoda',
+    height: 'short',
+    color: 'green',
+    force: function (){
+        console.log("yoda's short butt will kick yours");
+        return this.name.toUpperCase()+" says YAAAAAAAA!!!!!!!";
+    },
+    walk: function (){
+        console.log('hey, we ar wawkin heya')
+    }
+}
+
+let longString= "this is a very long string, however..."+
+    "it cant really move line to line"+ yoda.name;
+
+let longString2= `now this is a really
+long string
+that can move from line to line
+just like this person that I am using a template literal too access
+his name. That dudes name is ${yoda.name}`
+
+//anonymous function
+//Functions stored in variables do not need function names. 
+//  They are always invoked (called) using the variable name.
+let funcHolder= function (){
+    console.log('this my function')
+    return "hero Bill"
+}
+
+//self invoking function
+/*
+The benefit of self-invoking functions 
+is that they enable us to execute code 
+once without cluttering the global namespace 
+(without declaring any globals)
+*/
+(function (myargs){
+    console.log(myargs)
+}("cheese"));
+
+//
+// nested function
+function birdNest(){
+    let ourVar= 7;
+    function babyBird(){
+        let ourOtherVar= 15;
+        console.log('babybird(): '+ ++ourVar);
+    }
+    //console.log("inside the parent function (nested)");
+    babyBird();
+    //console.log(ourOtherVar);//will not work
+}
+
