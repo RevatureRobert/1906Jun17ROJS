e933891d895bff066cb86d63b807988dd448d58c
connellrobert <robert.lel.connell@gmail.com>
2019-06-25 12:23:18 -0400

    new DOM stuff

--- js/DomManip.js
+++ js/DomManip.js
@@ -1,12 +1,11 @@
-function hello(){
-    alert("Better events are on the horizon")
+function hello() {
+  alert("Better events are on the horizon");
 }
 
+// window.onload=alert('hello');
 
-window.onload=alert('hello');
-
-let button = document.getElementById('button');
-button.addEventListener('mouseover', hello);
+let button = document.getElementById("button");
+button.addEventListener("mouseover", hello);
 
 /*
    other events:   onsubmit, onchange, click, dblclick, mouseup,
@@ -21,29 +20,28 @@ button.addEventListener('mouseover', hello);
        submit
 */
 
+let body = document.querySelector("body");
+body.addEventListener("mousemove", bodyFunc);
 
-
-let body = document.querySelector('body');
-body.addEventListener('mousemove', bodyFunc);
-
-function bodyFunc(eve){
-    // console.log(eve);
-    body.style.backgroundColor=`rgb(${eve.offsetX},${eve.offsetY},40)`;
+function bodyFunc(eve) {
+  // console.log(eve);
+  body.style.backgroundColor = `rgb(${eve.offsetX},${eve.offsetY},40)`;
 }
 
-let image = document.getElementById('image');
+let image = document.getElementById("image");
+console.log(document.body);
+image.addEventListener("mouseenter", changeImage);
 
-image.addEventListener('mouseenter', changeImage);
+let imageArray = [
+  "https://i.ytimg.com/vi/mTHchH9VRh0/hqdefault.jpg",
+  "https://images.pexels.com/photos/1619507/pexels-photo-1619507.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+];
 
-let imageArray=['https://i.ytimg.com/vi/mTHchH9VRh0/hqdefault.jpg',
-'https://images.pexels.com/photos/1619507/pexels-photo-1619507.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'];
-
-let x=0;
-function changeImage(){
-    x++;
-    image.setAttribute('src',imageArray[x%imageArray.length]);
+let x = 0;
+function changeImage() {
+  x++;
+  image.setAttribute("src", imageArray[x % imageArray.length]);
 }
-
 
 //Just one last question...
 /*
@@ -52,4 +50,46 @@ What is DOM???
     The document object Model is how the browser views the html.
     It is a virtual representation for the html structure
 
+
+    Three special things we can do with DOM Manipulation
+        Find Elements
+            document.getElementById(id)
+            document.getElementsByClassName(className)
+            document.getElementsByTagName(tagName)
+        Change Elements
+            element.innerHtml = new html content
+            element.attribute = new value
+            element.setAttribute(attribute , value)
+            element.style.property = new style
+        Add or Remove Elements
+            document.createElement(element)
+            element.appendChild(element)
+            element.removeChild(element)
+            element.replaceChild(element)
+            document.write(text)
 */
+
+//Adding content with innerHtml.
+let paraOne = document.getElementById("para1");
+paraOne.innerHTML = "salazar Salad";
+
+//creating elements with append child and createElement
+//      functions
+
+let sArr=["howdy",'partner','my', 'name', 'is',
+'holy', 'cow', 'there','is','a','snake','in','my','boot']
+
+let listOne = document.getElementById("list1");
+
+for (let x = 0; x < sArr.length; x++) {
+    let li = document.createElement("li");
+  li.style='color:blue';
+  li.className='liClass'
+  listOne.appendChild(li);
+  li.innerHTML = 'this is a js item: '+ sArr[x];
+}
+
+  console.log("this is an id ")
+  console.log(document.getElementById('list1'))
+  console.log(' this is what is returned from class name')
+  console.log(document.getElementsByClassName('liClass'))
